ddbf3e28fafe931a457e35bf495529fef0e5a448
sebbejohansson <sebbe.johansson1995@gmail.com>
2023-03-08 21:57:57 +0100

    Initial commit.

---
+++ src/Plugin.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ckeditor :editor="editor" v-model="model.content" :config="editorConfig"></ckeditor>
+  </div>
+</template>
+
+<script>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+export default {
+  mixins: [window.Storyblok.plugin],
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorConfig: {
+        // The configuration of the editor. https://ckeditor.com/docs/ckeditor5/latest/installation/frameworks/vuejs-v2.html#config
+      }
+    };
+  },
+  methods: {
+    initWith() {
+      return {
+        plugin: 'ckeditor',
+        content: '',
+      };
+    },
+  },
+  watch: {
+    'model': {
+      handler: function (value) {
+        this.$emit('changed-model', value);
+      },
+      deep: true
+    }
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import CKEditor from '@ckeditor/ckeditor5-vue2';
+import Plugin from './Plugin.vue'
+
+window.Storyblok.vue.use(CKEditor);
+if (process.env.NODE_ENV == 'development') {
+
+  window.Fieldtype = Plugin
+  let customComp = window.Storyblok.vue.extend(window.Fieldtype);
+  window.Storyblok.vue.component('custom-plugin', customComp);
+  window.StoryblokPluginRegistered = true;
+
+} else {
+  
+  let init = Plugin.methods.initWith() 
+  window.storyblok.field_types[init.plugin] = Plugin
+
+}
+
